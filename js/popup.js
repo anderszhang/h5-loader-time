a871f9351fbf54a4a9c6f5b190f4bc346aa6c534
anderszhang <anderszhang@gmail.com>
2013-10-16 19:35:02 +0800

    add bootstrap

--- js/popup.js
+++ js/popup.js
@@ -2,13 +2,10 @@
  * Created by shbzhang on 13-10-16.
  */
 $(function(){
-//  console.log('ddd');
-//  $('#start').click(function(){
     chrome.tabs.create({
       url:'html/config.html',
       active: true
     }, function (){
 
     });
-//  });
 });
